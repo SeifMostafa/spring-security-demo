73dc5c80c8e85bfd55e46ad18f99f36bd123b699
Seif . Mostafa <seif.mostafa@MUSALA.COM>
2022-02-15 14:45:43 +0200

    finished security through db, roles, encrypted password handled

--- src/main/java/demo/config/DemoAppConfig.java
+++ src/main/java/demo/config/DemoAppConfig.java
@@ -1,23 +1,71 @@
 package demo.config;
+
+import java.beans.PropertyVetoException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+	@Autowired
+	private Environment env;
 
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		
+
 		viewResolver.setPrefix("/WEB-INF/view/");
 		viewResolver.setSuffix(".jsp");
-		
+
 		return viewResolver;
 	}
+
+	@Bean
+	public DataSource securityDataSource() {
+
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+
+			securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+			securityDataSource.setUser(env.getProperty("jdbc.user"));
+			securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+			securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+
+			securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+
+			securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+
+			securityDataSource.setMaxIdleTimeExcessConnections(getIntProperty("connection.pool.maxIdleTime"));
+
+		} catch (PropertyVetoException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		return securityDataSource;
+
+	}
+
+	private int getIntProperty(String propName) {
+		String propVal = env.getProperty(propName);
+		return Integer.parseInt(propVal);
+	}
+
 }
--- src/main/java/demo/config/DemoSecurityConfig.java
+++ src/main/java/demo/config/DemoSecurityConfig.java
@@ -1,26 +1,26 @@
 package demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	@Autowired
+	private DataSource datasource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		UserBuilder users = User.withDefaultPasswordEncoder();
-
-		auth.inMemoryAuthentication().withUser(users.username("csseifms").password("123456").roles("EMPLOYEE"))
-		.withUser(users.username("rashmelonjr").password("1234567").roles("EMPLOYEE"))
-		.withUser(users.username("basset").password("123").roles("MANAGER"))
-		.withUser(users.username("maged").password("1").roles("ADMIN"));
+		auth.jdbcAuthentication().dataSource(datasource);
 		
 	}
 
