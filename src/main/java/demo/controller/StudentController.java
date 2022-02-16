2ee2e1593c5ac5b9cb10695153e12fbf421fd79f
Seif . Mostafa <seif.mostafa@MUSALA.COM>
2022-02-16 11:02:20 +0200

    get student by id(array index), PathVariable.

--- src/main/java/demo/controller/StudentController.java
+++ src/main/java/demo/controller/StudentController.java
@@ -3,7 +3,10 @@ package demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +16,23 @@ import demo.entity.Student;
 @RequestMapping("/api")
 public class StudentController {
 
+	private List<Student> students;
+
+	@PostConstruct
+	public void loadData() {
+		students = new ArrayList<>();
+		students.add(new Student("john", "john"));
+		students.add(new Student("seif", "mostafa"));
+		students.add(new Student("omar", "gamal"));
+	}
+
 	@GetMapping("/students")
 	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<>();
-		students.add(new Student("john","john"));
-		students.add(new Student("seif","mostafa"));
-		students.add(new Student("omar","gamal"));
 		return students;
+	}
+
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		return students.get(studentId);
 	}
 }
