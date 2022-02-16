cd3a1eeb67b6c11d8b39c3a982873f7f07525fa0
Seif . Mostafa <seif.mostafa@MUSALA.COM>
2022-02-16 13:41:39 +0200

    generic exp handle, end of security CRM spring and to spring normal.

--- src/main/java/demo/controller/StudentController.java
+++ src/main/java/demo/controller/StudentController.java
@@ -5,16 +5,12 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import demo.entity.Student;
-import demo.exception.StudentErrorResponse;
 import demo.exception.StudentNotFoundException;
 
 @RestController
@@ -44,12 +40,6 @@ public class StudentController {
 		return students.get(studentId);
 	}
 
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exp) {
-		StudentErrorResponse error = new StudentErrorResponse();
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exp.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.NOT_FOUND);
-	}
+	
+	
 }
---
+++ src/main/java/demo/exception/StudentExceptionHandler.java
@@ -0,0 +1,31 @@
+package demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class StudentExceptionHandler {
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exp) {
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exp.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.NOT_FOUND);
+	}
+
+	/*
+	 * generic exception handle
+	 */
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exp) {
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exp.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.BAD_REQUEST);
+	}
+}
