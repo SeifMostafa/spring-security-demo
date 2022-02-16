5bf18fb00c4bf205ab6714495fee0580b51ec072
Seif . Mostafa <seif.mostafa@MUSALA.COM>
2022-02-16 13:01:26 +0200

    exp handle

--- src/main/java/demo/controller/StudentController.java
+++ src/main/java/demo/controller/StudentController.java
@@ -5,12 +5,17 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import demo.entity.Student;
+import demo.exception.StudentErrorResponse;
+import demo.exception.StudentNotFoundException;
 
 @RestController
 @RequestMapping("/api")
@@ -33,6 +38,18 @@ public class StudentController {
 
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
+		if (studentId >= students.size() || studentId < 0) {
+			throw new StudentNotFoundException("student id: " + studentId + " not found");
+		}
 		return students.get(studentId);
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exp) {
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exp.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.NOT_FOUND);
 	}
 }
